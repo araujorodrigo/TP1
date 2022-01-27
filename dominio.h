823db7fe81f3e9a6e68ae65ae6b7288c717176f2
Rodrigo Araujo <rodrygoalvys@gmail.com>
2022-01-27 15:25:02 -0300

    Declaração classe CIDADE

--- dominio.h
+++ dominio.h
@@ -1,6 +1,16 @@
 #ifndef DOMINIO_H_INCLUDED
 #define DOMINIO_H_INCLUDED
 
+class CIDADE{
+    private:
+        char    cidadeNome[15];
+        void    validar(int);
+
+    public:
+        void    setCidade();
+        char    getCidade(); const //Deve ser somente leitura
+
+}; // CIDADE
 
 
 #endif // DOMINIO_H_INCLUDED
